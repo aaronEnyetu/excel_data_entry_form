e7817e25f011ff5ddf1111682b83caa39087217d
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-06 10:20:03 -0800

    created function to load data

--- data_entry.py
+++ data_entry.py
@@ -2,17 +2,17 @@ from pathlib import Path
 import PySimpleGUI as sg
 import pandas as pd
 
+# Add some color to the window
+sg.theme('DarkTeal9')
 
+current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
+EXCEL_FILE = current_dir / 'Data_Entry.xlsx'
 
-
-
-
-
-
-
-
-
-
+# Load the data if the file exists, if not, create a new DataFrame
+if EXCEL_FILE.exists():
+    df = pd.read_excel(EXCEL_FILE)
+else:
+    df = pd.DataFrame()
 
 layout = [
     [sg.Text('Please fill out the following fields:')],
@@ -30,21 +30,24 @@ layout = [
 
 window = sg.Window('Simple data entry form', layout)
 
+def clear_input():
+    for key in values:
+        window[key]('')
+    return None
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Clear':
+        clear_input()
+    if event == 'Submit':
+        new_record = pd.DataFrame(values, index=[0])
+        df = pd.concat([df, new_record], ignore_index=True)
+        df.to_excel(EXCEL_FILE, index=False)  # This will create the file if it doesn't exist
+        sg.popup('Data saved!')
+        clear_input()
 
 
 
